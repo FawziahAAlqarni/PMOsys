819addfe3acadeeb4f926df80879a8ec38bcfaa3
Asim Alamri <aalamri@mngdp.com>
2025-12-08 12:51:53 +0300

    FE: ADD: fouzyah foundation

---
+++ fe/src/app/fouzyah-option/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React, {useState, useEffect} from 'react';
+import Navbar from '@/components/Navbar';
+import PortfolioView from '@/components/PortfolioView';
+import AnalyticsView from '@/components/AnalyticsView';
+import ProjectDetailsView from '@/components/ProjectDetailsView';
+import NewProjectModal from '@/components/modals/NewProjectModal';
+import CharterModal from '@/components/modals/CharterModal';
+import RiskRegisterModal from '@/components/modals/RiskRegisterModal';
+import {GATES_TEMPLATE} from '@/lib/constants';
+
+export default function Home() {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
